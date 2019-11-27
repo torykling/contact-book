025e563a5319c9921d64fa520730b08d36404c5d
Tory Klingenstein <toryklingenstein@gmail.com>
2019-11-27 11:09:51 -0500

    add contacts

--- lib/main.py
+++ lib/main.py
@@ -1,49 +1,68 @@
 from peewee import *
-from datetime import date
 
-# create db object with configuration for database
-db = PostgresqlDatabase('people', user='postgres',
+db = PostgresqlDatabase('contacts', user='postgres',
                         password='', host='localhost', port='5432')
 
-# connect to db
+
 db.connect()
 
-# a meta class is a class defined in a class that we use for configuration to the database
+
 class BaseModel(Model):
     class Meta:
         database = db
 
-# subclass of the base model to create a person model
-class Person(BaseModel):
-    name = CharField()
-    birthday = DateField()
 
-class Pet(BaseModel):
-  name = CharField()
-  animal_type = CharField()
-# create tables in database for models
-db.create_tables([Person, Pet])
+class Contact(BaseModel):
+    first_name = CharField()
+    last_name = CharField()
+    phone = CharField()
+    email = CharField()
 
 
-tory = Person(name='Tory', birthday=date(1988, 8, 3))
-tory.save()
-greta = Pet(name='Greta', animal_type='dog')
-greta.save()
+db.create_tables([Contact])
 
-# two ways to read -- .get() returns only the first record
-Pet.get(Pet.name == 'Greta')
-Pet.get(Pet.animal_type == 'dog')
+show = input("Would you like to see all of your contacts? y/n")
+if show == 'y':
+    all_contacts = Contact.select()
+    # print()
 
-# .select() - gets multiple records
-# this gets them all
-Person.select()
-# this gets by condition
-Person.select().where(Person.birthday < date(1990, 1, 1))
+add = input("Add a contact? y/n")
+if add == 'y':
 
-# update (if we didn't already have this variable defines)
-tory = Person.get(Person.name == 'Tory')
-tory.birthday = date(1988, 8, 4)
-tory.save()
+    new_first = input("What is this contact's first name?")
+    new_last = input("What is this contact's last name?")
+    new_phone = input("What is this contact's phone number?")
+    new_email = input("What is this contact's email?")
+    new_contact = Contact(first_name=new_first,
+                          last_name=new_last, phone=new_phone, email=new_email)
+    new_contact.save()
 
-# delete
-tory.delete_instance()
+search = input("Would you like to search for a contact by first name? y/n")
+if search == 'y':
+    search_name = input(
+        "What is the first name of the contact you'd like to find?")
+
+    search_result = Contact.get(Contact.first_name == search_name)
+    print(f'first name: {search_result.first_name}\n last name: {search_result.last_name}\n phone: {search_result.phone}\n email: {search_result.email}')
+    update = input("Would you like to update this contact? y/n")
+    if update == 'y':
+        updated_first = input(
+            "What is the updated first name of this contact?")
+        updated_last = input(
+            "What is the updated last name of this contact?")
+        updated_phone = input(
+            "What is the updated phone number for this contact?")
+        updated_email = input(
+            "What is the updated email for this contact?")
+        search_result.first_name = updated_first
+        search_result.last_name = updated_last
+        search_result.phone = updated_phone
+        search_result.email = updated_email
+        search_result.save()
+    else:
+        delete = input("Would you like to delete this contact? y/n")
+        if delete == 'y':
+            search_result.delete_instance()
+
+
+# switch to oop!
